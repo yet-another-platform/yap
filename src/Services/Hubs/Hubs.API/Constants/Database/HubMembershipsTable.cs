b952a0ed6860f996f751f8e26dba4e51272d0fe7
Dalibor Dřevojánek <dalibor.drevojanek@elixeum.com>
2025-10-06 19:30:17 +0200

    Add models

---
+++ src/Services/Hubs/Hubs.API/Constants/Database/HubMembershipsTable.cs
@@ -0,0 +1,6 @@
+namespace Hubs.API.Constants.Database;
+
+public static class HubMembershipsTable
+{
+    public const string TableName = "hub_memberships";
+}
---
+++ src/Services/Hubs/Hubs.API/Constants/Database/HubsTable.cs
@@ -0,0 +1,6 @@
+namespace Hubs.API.Constants.Database;
+
+public static class HubsTable
+{
+    public const string TableName = "hubs";
+}
